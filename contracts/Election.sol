d3cf1f77b57afca59f2f0ff5f6020c8b048f0084
bekhtinartem <bekhtin_av_10_20@1511.ru>
2022-10-22 18:17:27 +0300

    start

---
+++ contracts/Election.sol
@@ -0,0 +1,50 @@
+pragma solidity 0.5.16;
+
+contract Election {
+    
+    // Model a Messege 
+    struct Messege{
+        uint idMessage;
+        string text;
+        uint userFrom;
+        uint userTo;
+        
+    }
+    mapping(uint => Messege) public messanges;
+    // Model a User
+    struct User {
+        uint id;
+        string name;
+        uint countMessages;
+    }
+
+    // Read/write users
+    mapping(uint => User) public users;
+    
+
+    // Store Users Count
+    uint public usersCount=0;
+    uint public countMessages=0;
+    constructor () public {
+        //set default users
+        addUser('1');
+        addUser('2');
+    }
+
+    function addUser (string memory _name) private {
+        usersCount++;
+        users[usersCount]=User(usersCount, _name, 0);
+        
+    }
+
+    function sendMessage (uint _userFrom, uint _userTo, string memory _message) public {
+        require(
+            _userFrom > 0 &&
+            _userFrom <= usersCount &&
+            _userTo > 0 &&
+            _userTo <= usersCount);
+            countMessages++;
+            messanges[countMessages]=Messege(countMessages, _message, _userFrom, _userTo);
+        
+    }
+}
